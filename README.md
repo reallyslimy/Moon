4d02e84da0535f594f66243a19b8f39ffe8ff22a
reallyslimy <jesusvelezmar@gmail.com>
2022-12-14 13:35:44 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Neon Purple
+A custom handcrafted bot by Slimy.
+
+## Neon Purple Features
+- Monorepo
+- Linting
+- Code formatting
+- Handler Package preincluded
---
+++ bot/neonpurple/environment.d.ts
@@ -0,0 +1,12 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            DISCORD_TOKEN: string;
+            TEST_GUILD: string;
+            ENV: 'dev' | 'prod' | 'debug';
+            MONGO_URI: string;
+        }
+    }
+}
+
+export {};
