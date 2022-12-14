7a2d1ef12b5d6b2633c7027f93687bfc0e7c37f2
reallyslimy <jesusvelezmar@gmail.com>
2022-12-14 16:48:52 +0100

    Test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,40 +1,43 @@
 module.exports = {
-    parserPreset: {
-        parserOpts: {
-            headerPattern: /^(?:(:\w+:)\s)?\[(\w+)\] (.+)/,
-            headerCorrespondence: ["type", "scope", "subject"]
-        }
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(?:(:\w+:)\s)?\[(\w+)\] (.+)/,
+      headerCorrespondence: ['type', 'scope', 'subject'],
     },
-    plugins: [
-        {
-            rules: {
-                'header-match-team-pattern': (parsed) => {
-                    const { type, scope, subject} = parsed;
-                    if(type == null && scope == null && subject == null) {
-                        return [
-                            false,
-                            'Header must be in format :gitmoji:? [scope] subject',
-                        ];
-                    }
-                    return [true, ""]
-                },
-                "gitmoji-type-enum": (parsed, _when, expectedValue) => {
-                    const {type} = parsed;
-                    if(type && !expectedValue.includes(type)) {
-                        return [
-                            false,
-                            `type must be one of ${expectedValue}
+  },
+  plugins: [
+    {
+      rules: {
+        'header-match-team-pattern': (parsed) => {
+          const {type, scope, subject} = parsed
+          if (type == null && scope == null && subject == null) {
+            return [
+              false,
+              'Header must be in format :gitmoji:? [scope] subject',
+            ]
+          }
+          return [true, '']
+        },
+        'gitmoji-type-enum': (parsed, _when, expectedValue) => {
+          const {type} = parsed
+          if (type && !expectedValue.includes(type)) {
+            return [
+              false,
+              `type must be one of ${expectedValue}
                             see https://gitmoji.dev`,
-                        ];
-                    }
-                    return [true, ""]
-                }
-            }
-        }
+            ]
+          }
+          return [true, '']
+        },
+      },
+    },
+  ],
+  rules: {
+    'header-match-team-pattern': [2, 'always'],
+    'gitmoji-type-enum': [
+      2,
+      'always',
+      [':bug:', ':sparkles:', ':ambulance:', ':art:', ':zap:', ':lock:'],
     ],
-    rules: {
-        "header-match-team-pattern": [2, "always"],
-        "gitmoji-type-enum": [2, "always", [":bug:", ":sparkles:", ":ambulance:", ":art:", ":zap:", ":lock:"]]
-    }
-
+  },
 }
